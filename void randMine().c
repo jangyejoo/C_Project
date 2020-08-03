be30381bbbe2d7738a977e06f85221f9716cf71d
jangyejoo <jyj01068662778@gmail.com>
2020-08-03 20:54:02 +0900

    Add Text File [void randMine().c]

---
+++ void randMine().c
@@ -0,0 +1,15 @@
+void randMine(struct mineboard(*m)[MAX], int gamelevel) {
+	int mineCnt=0;
+	if (gamelevel == 15) mineCnt = 10;
+	else if (gamelevel == 20) mineCnt = 50;
+	else if (gamelevel == 25) mineCnt = 80;
+	do {
+		int i, j;
+		i = randx(gamelevel);
+		j = randx(gamelevel);
+		if (m[i][j].mine == 0) {
+			m[i][j].mine = 1;
+			mineCnt--;
+		}
+	} while (mineCnt);
+}
