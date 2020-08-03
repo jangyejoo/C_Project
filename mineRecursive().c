b5cfcda1c210f880e1d7b0d542fa7b2faa3be3eb
Yuchaeng <shub29@naver.com>
2020-08-04 02:52:38 +0900

    Add void mineRecursive().c

---
+++ mineRecursive().c
@@ -0,0 +1,16 @@
+void mineRecursive(struct mineboard(*m)[MAX_Y], int tmpX, int tmpY){
+	int i, j;
+	if (m[tmpX][tmpY].block == 1) return;
+	else if (m[tmpX][tmpY].wall == 1) return;
+	else if (m[tmpX][tmpY].minecount != 0) {
+		m[tmpX][tmpY].block = 1;
+		return;
+	}
+	else if (m[tmpX][tmpY].minecount == 0) {
+		for (i = tmpX-2; i < tmpX + 4; i++) {
+			for (j = tmpY - 1; j < tmpY + 2; j++) {
+				mineRecursive(m, tmpX, tmpY);
+			}
+		}
+	}
+}
