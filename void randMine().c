56d2abcb2c2b899992b8fad7839461ff3497f04f
Yuchaeng <shub29@naver.com>
2020-08-05 14:43:54 +0900

    Add void randMine().c

--- void randMine().c
+++ void randMine().c
@@ -3,10 +3,11 @@ void randMine(struct mineboard(*m)[MAX], int gamelevel) {
 	if (gamelevel == 15) mineCnt = 10;
 	else if (gamelevel == 20) mineCnt = 50;
 	else if (gamelevel == 25) mineCnt = 80;
+	srand(time(NULL));
 	do {
 		int i, j;
-		i = randx(gamelevel);
-		j = randx(gamelevel);
+		i = rand() % (gamelevel - 2) + 1;
+		j = rand() % (gamelevel - 2) + 1;
 		if (m[i][j].mine == 0) {
 			m[i][j].mine = 1;
 			mineCnt--;
