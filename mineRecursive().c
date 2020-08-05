3759e8590b6fae44a18743b9448ede6f2630fae8
jangyejoo <jyj01068662778@gmail.com>
2020-08-06 00:53:11 +0900

    Edit Text File [mineRecursive()]

--- mineRecursive().c
+++ mineRecursive().c
@@ -6,8 +6,9 @@ void mineRecursive(struct mineboard(*m)[MAX_Y], int tmpX, int tmpY){
 		m[tmpX][tmpY].block = 1;
 		return;
 	}
-	else if (m[tmpX][tmpY].minecount == 0) {
-		for (i = tmpX-2; i < tmpX + 4; i++) {
+	else {
+		m[tmpX][tmpY].block=1;
+		for (i = tmpX-1; i < tmpX + 2; i++) {
 			for (j = tmpY - 1; j < tmpY + 2; j++) {
 				mineRecursive(m, i, j);
 			}
