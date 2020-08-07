0b32ccf5d0f301f2ea3a146a6e8a6f13209307a4
Yuchaeng <shub29@naver.com>
2020-08-07 13:14:31 +0900

    int main().c]

--- int main().c
+++ int main().c
@@ -5,8 +5,10 @@ int Lv1[3], Lv2[3], Lv3[3]; //기록변수 배열
 
 int main() {
 	struct mineboard m[MAX_X][MAX_Y];
-	int i, ch, select, elapsedTime;
+	int i, select, elapsedTime;
+	char c1;
 	int gamelevel = 1;
+
 	FILE* fp_r1 = fopen("easy.txt", "r");
 	FILE* fp_r2 = fopen("mid.txt", "r");
 	FILE* fp_r3 = fopen("hard.txt", "r");
@@ -14,19 +16,27 @@ int main() {
 	if (fp_r1 == NULL) {
 		for (i = 0; i < 3; i++) Lv1[i] = 999;
 	}
+	else {
+		for (i = 0; i < 3; i++) fscanf(fp_r1, "%d", &Lv1[i]);
+		fclose(fp_r1);
+	}
+
 	if (fp_r2 == NULL) {
 		for (i = 0; i < 3; i++) Lv2[i] = 999;
 	}
+	else {
+		for (i = 0; i < 3; i++) fscanf(fp_r2, "%d", &Lv2[i]);
+		fclose(fp_r2);
+	}
+
 	if (fp_r3 == NULL) {
 		for (i = 0; i < 3; i++) Lv3[i] = 999;
 	}
-
-	for (i = 0; i < 3; i++) fscanf(fp_r1, "%d", Lv1[i]);
-	for (i = 0; i < 3; i++) fscanf(fp_r2, "%d", Lv2[i]);
-	for (i = 0; i < 3; i++) fscanf(fp_r3, "%d", Lv3[i]);
-
+	else {
+		for (i = 0; i < 3; i++) fscanf(fp_r3, "%d", &Lv3[i]);
+		fclose(fp_r3);
+	}
 	
-
 	while (1) {
 		system("cls");
 		mainMenu();
@@ -43,7 +53,7 @@ int main() {
 			case 15:
 				if (elapsedTime < Lv1[2]) {
 					printf("기록을 저장하시겠습니까? y / n");
-					char c1 = getchar();
+					c1 = getchar();
 					if (c1 == 'y') {
 						if (elapsedTime > Lv1[1]) Lv1[2] = elapsedTime;
 						else if (elapsedTime < Lv1[0]) Lv1[0] = elapsedTime;
@@ -52,12 +62,11 @@ int main() {
 					}
 					else if (c1 == 'n') break;
 				}
-				break;
 
 			case 20:
 				if (elapsedTime < Lv2[2]) {
 					printf("기록을 저장하시겠습니까? y / n");
-					char c1 = getchar();
+					c1 = getchar();
 					if (c1 == 'y') {
 						if (elapsedTime > Lv2[1]) Lv2[2] = elapsedTime;
 						else if (elapsedTime < Lv2[0]) Lv2[0] = elapsedTime;
@@ -66,12 +75,11 @@ int main() {
 					}
 					else if (c1 == 'n') break;
 				}
-				break;
 
 			case 25:
 				if (elapsedTime < Lv3[2]) {
 					printf("기록을 저장하시겠습니까? y / n");
-					char c1 = getchar();
+					c1 = getchar();
 					if (c1 == 'y') {
 						if (elapsedTime > Lv3[1]) Lv3[2] = elapsedTime;
 						else if (elapsedTime < Lv3[0]) Lv3[0] = elapsedTime;
@@ -80,7 +88,6 @@ int main() {
 					}
 					else if (c1 == 'n') break;
 				}
-				break;
 			} //기록 저장 묻기 끝
 
 		case 2: //순위 보기
