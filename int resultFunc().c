8dd9d1e2d3a18aae13d3abe19001349dfceb392f
Yuchaeng <shub29@naver.com>
2020-08-07 12:11:17 +0900

    Edit [int resultFunc().c]

--- int resultFunc().c
+++ int resultFunc().c
@@ -1,9 +1,7 @@
 int resultFunc(struct mineboard(*m)[MAX_Y], int gamelevel, int allMine){
 	int count = 0;
 	int i, j;
-	if (gamelevel == 15) allMine = 10;
-	else if (gamelevel == 20) allMine = 50;
-	else if (gamelevel == 25) allMine = 80;
+	
 	for (i = 0; i < gamelevel; i++) {
 		for (j = 0; j < gamelevel; j++) {
 			if (m[i][j].wall != 1 && m[i][j].block == 0)
@@ -12,9 +10,9 @@ int resultFunc(struct mineboard(*m)[MAX_Y], int gamelevel, int allMine){
 	}
 	if (allMine == count) {
 		displayMap(m, gamelevel);
-		gotoxy(gamelevel * 2, 6);
+		gotoxy(gamelevel * 2 + 5, 6);
 		printf("게임 성공!");
-		gotoxy(gamelevel * 2, 7);
+		gotoxy(0, gamelevel + 5);
 		return 1;
 	}
 	return 0;
