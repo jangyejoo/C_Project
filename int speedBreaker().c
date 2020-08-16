7e25473c684bebb4dacaa81dcbb7cbf8861ca824
jangyejoo <jyj01068662778@gmail.com>
2020-08-16 22:24:56 +0900

    Add Text File [speedBreaker()]

---
+++ int speedBreaker().c
@@ -0,0 +1,18 @@
+int speedBreaker(struct mineboard(*m)[MAX], int tmpX, int tmpY) {
+	int i, j;
+	int result=1;
+	bool exitOuterLoop = false;
+
+	for (i = tmpX - 1; i < tmpX + 2; i++) {
+		for (j = tmpY - 1; j < tmpY + 2; j++) {
+			if (m[i][j].mine == 1 && m[i][j].block == 0) {
+				if(m[i][j].flag==0)
+					result*= 0;
+			}
+			else continue;
+		}
+	}
+
+	return result;
+	
+}
