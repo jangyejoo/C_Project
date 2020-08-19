e2338095a80cbf26ca297ff6f142b7f8e1c2c6cb
jangyejoo <jyj01068662778@gmail.com>
2020-08-19 21:59:45 +0900

    Edit [full code 5.c]

--- full code 5.c
+++ full code 5.c
@@ -390,6 +390,20 @@ int gameStart(struct mineboard(*m)[MAX], int gamelevel,int *retMine) { //매개변�
 					}
 
 				}
+				else {
+					for (i = 0; i < gamelevel; i++) {
+						for (j = 0; j < gamelevel; j++) {
+							if (m[i][j].wall != 1) m[i][j].block = 1;
+						}
+					}
+					system("cls");
+					displayMap(m, gamelevel);
+					gotoxy(gamelevel * 2 + 5, 6);
+					printf("게임 실패");
+					gotoxy(0, gamelevel + 3);
+					system("pause");
+					return 999;
+				}
 
 			}
 		}
