ba337fc9989aa4656b24c9671ad81da2e7e7283a
Yuchaeng <shub29@naver.com>
2020-08-20 18:32:35 +0900

    Edit [int speedUnlock().c]

--- int speedUnlock().c
+++ int speedUnlock().c
@@ -1,22 +1,15 @@
-int speedBreaker(struct mineboard(*m)[MAX], int tmpX, int tmpY) {
+int speedUnlock(struct mineboard(*m)[MAX], int tmpX, int tmpY) {
 	int i, j;
-	int result=1;
-	bool exitOuterLoop = false;
+	int result = 1;
+	int count = 0;
 
 	for (i = tmpX - 1; i < tmpX + 2; i++) {
 		for (j = tmpY - 1; j < tmpY + 2; j++) {
-			if (m[i][j].mine == 1 && m[i][j].block == 0) {
-				if(m[i][j].flag==0)
-					result*= 0;
-			}
-			else if (m[i][j].mine==0) {
-				if (m[i][j].flag==1)
-					result*=0;
-			}
-			else continue;
+			if (m[i][j].flag == 1) count++;
 		}
 	}
+	if (m[tmpX][tmpY].minecount == count) return result;
+	else if (count == 0) return result *= 0;
 
-	return result;
-	
+	return -1;
 }
