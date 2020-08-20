fa06f27f65ea42bfa124b962f154713c490efe7e
Yuchaeng <shub29@naver.com>
2020-08-20 18:21:27 +0900

    Edit [void rank().c]

--- void rank().c
+++ void rank().c
@@ -1,21 +1,39 @@
 void rank() {
+	int i;
 	puts("▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩");
-	puts("▩                          ▩");
-	puts("▩     ※ 랭킹 TOP3 ※      ▩");
-	puts("▩                          ▩");
-	puts("▩                          ▩");
-	puts("▩          *초급*          ▩");
-	puts("▩                          ▩");
-	printf("▩   %3d     %3d     %3d    ▩\n",Lv1[0],Lv1[1],Lv1[2]);
-	puts("▩                          ▩");
-	puts("▩          *중급*          ▩");
-	puts("▩                          ▩");
-	printf("▩   %3d     %3d     %3d    ▩\n",Lv2[0],Lv2[1],Lv2[2]);
-	puts("▩                          ▩");
-	puts("▩          *고급*          ▩");
-	puts("▩                          ▩");
-	printf("▩   %3d     %3d     %3d    ▩\n",Lv3[0],Lv3[1],Lv3[2]);
-	puts("▩                          ▩");
+	puts("");
+	puts("");
+	puts("       ※ 랭킹 TOP3 ※       ");
+	puts("");
+	puts("");
+	puts("");
+	puts("         ▼ 초급 ▼         ");
+	puts("");
+	for (i = 0; i < 3; i++) {
+		if (Lv1[i] == '\0') printf("          ");
+		else printf("   %3d    ", Lv1[i]);
+	}
+	puts("");
+	puts("");
+	puts("");
+	puts("         ▼ 중급 ▼         ");
+	puts("");
+	for (i = 0; i < 3; i++) {
+		if (Lv2[i] == '\0') printf("          ");
+		else printf("   %3d    ", Lv2[i]);
+	}
+	puts("");
+	puts("");
+	puts("");
+	puts("         ▼ 고급 ▼          ");
+	puts("");
+	for (i = 0; i < 3; i++) {
+		if (Lv3[i] == '\0') printf("          ");
+		else printf("   %3d    ", Lv3[i]);
+	}
+	puts("");
+	puts("");
+	puts("");
 	puts("▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩");
 	puts("");
 }
