efa8221ede969c7a2886472681d6c45a09c6e998
Yuchaeng <shub29@naver.com>
2020-08-20 18:38:34 +0900

    Edit [int main().c]

--- int main().c
+++ int main().c
@@ -3,18 +3,16 @@
 #include <windows.h>
 int Lv1[3], Lv2[3], Lv3[3]; //기록변수 배열
 
-int main(int mineCnt) {
-	struct mineboard m[MAX_X][MAX_Y];
+int main() {
 	int i, select, elapsedTime;
-	char c1;
 	int gamelevel = 1;
 
 	FILE* fp_r1 = fopen("easy.txt", "r");
 	FILE* fp_r2 = fopen("mid.txt", "r");
 	FILE* fp_r3 = fopen("hard.txt", "r");
-	
+
 	if (fp_r1 == NULL) {
-		for (i = 0; i < 3; i++) Lv1[i] = 999;
+		for (i = 0; i < 3; i++) Lv1[i] = '\0';
 	}
 	else {
 		for (i = 0; i < 3; i++) fscanf(fp_r1, "%d", &Lv1[i]);
@@ -22,7 +20,7 @@ int main(int mineCnt) {
 	}
 
 	if (fp_r2 == NULL) {
-		for (i = 0; i < 3; i++) Lv2[i] = 999;
+		for (i = 0; i < 3; i++) Lv2[i] = '\0';
 	}
 	else {
 		for (i = 0; i < 3; i++) fscanf(fp_r2, "%d", &Lv2[i]);
@@ -30,13 +28,13 @@ int main(int mineCnt) {
 	}
 
 	if (fp_r3 == NULL) {
-		for (i = 0; i < 3; i++) Lv3[i] = 999;
+		for (i = 0; i < 3; i++) Lv3[i] = '\0';
 	}
 	else {
 		for (i = 0; i < 3; i++) fscanf(fp_r3, "%d", &Lv3[i]);
 		fclose(fp_r3);
 	}
-	
+
 	while (1) {
 		system("cls");
 		mainMenu();
@@ -44,91 +42,71 @@ int main(int mineCnt) {
 		system("cls");
 
 		switch (select) {
-		case 1: //겜시작
+		case 1:
 			gamelevel = levelSelect();
-				
-			switch (gamelevel) {  //기록 저장 묻기-레벨별로
+
+			switch (gamelevel) {
 			case 15:
 				makeMap(m, gamelevel);
 				system("cls");
-				elapsedTime = gameStart(m, gamelevel,&mineCnt);
-				if (elapsedTime < Lv1[2]) {
-					printf("기록을 저장하시겠습니까? y / n\n");
-					c1 = _getch();
-					if (c1 == 121) {
-						if (elapsedTime > Lv1[1]) {
-							Lv1[2] = elapsedTime;
-						}								
-						else if (elapsedTime < Lv1[0]) {
-							Lv1[2] = Lv1[1];
-							Lv1[1] = Lv1[0];
-							Lv1[0] = elapsedTime;
-						}
-						else {
-							Lv1[2] = Lv1[1];
-							Lv1[1] = elapsedTime;
-						}
+				elapsedTime = gameStart(m, gamelevel);
+				if (elapsedTime != -1) {
+					if (Lv1[2] == '\0') {
+						sort_zero(Lv1, elapsedTime);
 						break;
 					}
-					else if (c1 == 'n') break;
+					else if (Lv1[2] != '\0' && elapsedTime < Lv1[2]) {
+						sort(Lv1, elapsedTime);
+						break;
+					}
+					else if (Lv1[2] != '\0' && elapsedTime > Lv1[2]) {
+						break;
+					}
 				}
 				break;
 
 			case 20:
 				makeMap(m, gamelevel);
 				system("cls");
-				elapsedTime = gameStart(m, gamelevel, &mineCnt);
-				if (elapsedTime < Lv2[2]) {
-					printf("기록을 저장하시겠습니까? y / n\n");
-					c1 = _getch();
-					if (c1 == 121) {
-						if (elapsedTime > Lv2[1]) {
-							Lv2[2] = elapsedTime;
-						}
-						else if (elapsedTime < Lv2[0]) {
-							Lv2[2] = Lv2[1];
-							Lv2[1] = Lv2[0];
-							Lv2[0] = elapsedTime;
-						}
-						else {
-							Lv2[2] = Lv2[1];
-							Lv2[1] = elapsedTime;
-						}
+				elapsedTime = gameStart(m, gamelevel);
+				if (elapsedTime != -1) {
+					if (Lv2[2] == '\0') {
+						sort_zero(Lv2, elapsedTime);
 						break;
 					}
-					else if (c1 == 'n') break;
+					else if (Lv2[2] != '\0' && elapsedTime < Lv2[2]) {
+						sort(Lv2, elapsedTime);
+						break;
+					}
+					else if (Lv2[2] != '\0' && elapsedTime > Lv2[2]) {
+						break;
+					}
 				}
 				break;
+
 			case 25:
 				makeMap(m, gamelevel);
 				system("cls");
-				elapsedTime = gameStart(m, gamelevel, &mineCnt);
-				if (elapsedTime < Lv3[2]) {
-					printf("기록을 저장하시겠습니까? y / n\n");
-					c1 = _getch();
-					if (c1 == 121) {
-						if (elapsedTime > Lv3[1]) {
-							Lv3[2] = elapsedTime;
-						}
-						else if (elapsedTime < Lv3[0]) {
-							Lv3[2] = Lv3[1];
-							Lv3[1] = Lv3[0];
-							Lv3[0] = elapsedTime;
-						}
-						else {
-							Lv3[2] = Lv3[1];
-							Lv3[1] = elapsedTime;
-						}
+				elapsedTime = gameStart(m, gamelevel);
+				if (elapsedTime != -1) {
+					if (Lv3[2] == '\0') {
+						sort_zero(Lv3, elapsedTime);
 						break;
 					}
-					else if (c1 == 'n') break;
+					else if (Lv3[2] != '\0' && elapsedTime < Lv3[2]) {
+						sort(Lv3, elapsedTime);
+						break;
+					}
+					else if (Lv3[2] != '\0' && elapsedTime > Lv3[2]) {
+						break;
+					}
 				}
 				break;
-			} //기록 저장 묻기 끝
+			}
 			break;
 
-		case 2: //순위 보기
-			rank(); 
+		case 2:
+			rank();
 			system("pause");
 			break;
 
@@ -136,9 +114,11 @@ int main(int mineCnt) {
 			howtoPlay();
 			system("pause");
 			break;
-		case 4: //겜종료, 기록 저장
+
+		case 4:
 			gameLog();
 			return 0;
+
 		default:
 			break;
 		}
